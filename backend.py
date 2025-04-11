f53160edd301368a13ee6f9a4920a11f4410e24d
Devansh Shah <devanshshah2019@gecg28.ac.in>
2025-04-11 18:05:09 -0500

    Initial commit - Flask bg remover app

---
+++ backend.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, send_file, render_template, url_for
+from rembg import remove
+from PIL import Image
+import io
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+PROCESSED_FOLDER = 'static/processed'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(PROCESSED_FOLDER, exist_ok=True)
+
+output_image_path = None
+
+@app.route('/')
+def index():
+    return render_template('index.html', output_image=output_image_path)
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+    global output_image_path
+    if 'image' not in request.files:
+        return "No file uploaded", 400
+    
+    file = request.files['image']
+    if file.filename == '':
+        return "No selected file", 400
+    output_image=None
+    input_image = Image.open(file.stream)
+    print(input_image)
+    output_image = remove(input_image)
+    print(output_image)
+    output_image_path = os.path.join(PROCESSED_FOLDER, 'output.png')
+    output_image.save(output_image_path, format='PNG')
+    print(output_image_path)
+    image_url = url_for('static', filename='processed/output.png') if output_image else None
+    return render_template('result.html', output_image=image_url)
+
+    
+
+@app.route('/download')
+def download():
+    if output_image_path:
+        return send_file(output_image_path, mimetype='image/png', as_attachment=True, download_name='output.png')
+    return "No image available", 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
